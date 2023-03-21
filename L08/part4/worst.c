e5465a3aea9f6e3e1f81678dc493ad8c136891c8
Gurman Johal <gurmanjohal@Gurmans-MacBook-Pro.local>
2023-03-21 17:36:33 -0400

    renamed and done

---
+++ L08/part4/worst.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int arr[2] = {1, 2};
+    int index = 3;
+    int flag = 0;
+
+    int result = 1 / 0; // zero division
+
+    flag++;  // incrementing a Boolean variable
+
+    if (flag == 1) {
+        printf("Flag is true!\n");
+    } else if (flag == 1) { // matching if and else if conditions
+        printf("Flag is still true!\n");
+    } else if (flag == 0) {
+        printf("Flag is false!\n");
+    }
+
+    printf("Value at index 3: %d\n", arr[index]); // out of bounds array access
+
+    return 0;
+}
